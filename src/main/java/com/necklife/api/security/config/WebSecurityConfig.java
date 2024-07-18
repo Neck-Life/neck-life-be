ff28c16be64dca949f0a45fb13b1e09aa61ad10f
Moon-1C <dudtkd665@gmail.com>
2024-07-19 01:02:15 +0900

    fix: spotlessApply관련 오류 수정 및 테스트 비활성화
    <description>
    <issue-number>

--- src/main/java/com/necklife/api/security/config/WebSecurityConfig.java
+++ src/main/java/com/necklife/api/security/config/WebSecurityConfig.java
@@ -116,7 +116,7 @@ public class WebSecurityConfig {
 								"/openapi3.yaml",
 								"/reports/**",
 								"/**")
-						.requestMatchers(HttpMethod.POST, "/api/v1/members");
+						.requestMatchers(HttpMethod.POST, "/api/v1/members", "/api/v1/members/basic");
 	}
 
 	@Bean
