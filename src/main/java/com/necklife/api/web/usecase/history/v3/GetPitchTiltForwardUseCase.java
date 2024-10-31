3cb81e1f9e757273665a040b971bcc4891542f52
Moon-1C <dudtkd665@gmail.com>
2024-10-31 15:18:18 +0900

    fix!!: localdataTime 오류
    <description>
    <issue-number>

--- src/main/java/com/necklife/api/web/usecase/history/v3/GetPitchTiltForwardUseCase.java
+++ src/main/java/com/necklife/api/web/usecase/history/v3/GetPitchTiltForwardUseCase.java
@@ -31,7 +31,7 @@ public class GetPitchTiltForwardUseCase {
 				historySummaryRepository.findByMemberIdAndDateBetweenOrderByDate(
 						memberId,
 						LocalDate.of(year, month, 1),
-						LocalDate.of(year, month, 1).plusMonths(1).minusDays(1));
+						LocalDate.of(year, month, 1).plusMonths(1));
 
 		// 요약된 데이터를 바탕으로 일별 데이터 변환
 		List<GetMonthlyDetailPitchForwardTiltResponse.Day> daily =
