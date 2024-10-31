90d8c018d6ba3a5805f2bcafb9690f02754726ba
Moon-1C <dudtkd665@gmail.com>
2024-10-31 23:15:14 +0900

    feat : FCMInitializer 가 절대경로 값으로 읽도록 수정
    <description>
    <issue-number>

--- src/main/java/com/necklife/api/notification/FCMInitializer.java
+++ src/main/java/com/necklife/api/notification/FCMInitializer.java
@@ -4,11 +4,12 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -20,9 +21,7 @@ public class FCMInitializer {
 
 	@PostConstruct
 	public void getFcmCredential() {
-		try {
-			InputStream refreshToken = new ClassPathResource(fcmKeyPath).getInputStream();
-
+		try (InputStream refreshToken = new FileInputStream(fcmKeyPath)) { // FileInputStream으로 절대 경로 파일 읽기
 			FirebaseOptions options =
 					FirebaseOptions.builder()
 							.setCredentials(GoogleCredentials.fromStream(refreshToken))
@@ -31,7 +30,7 @@ public class FCMInitializer {
 			FirebaseApp.initializeApp(options);
 			log.info("Fcm Setting Completed");
 		} catch (IOException e) {
-			throw new RuntimeException(e.getMessage());
+			throw new RuntimeException("Failed to initialize FCM", e);
 		}
 	}
 }
