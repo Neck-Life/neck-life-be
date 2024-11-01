75552fad4f10c6db15f1745935c385d90c1c08f4
Moon-1C <dudtkd665@gmail.com>
2024-11-01 14:49:35 +0900

    Fix!!: history 오류 수정
    <description>
    <issue-number>

--- src/main/java/com/necklife/api/web/usecase/history/GetMonthlyDetailUseCase.java
+++ src/main/java/com/necklife/api/web/usecase/history/GetMonthlyDetailUseCase.java
@@ -26,7 +26,9 @@ public class GetMonthlyDetailUseCase {
 
 		List<HistorySummaryEntity> historySummaryEntities =
 				historySummaryRepository.findByMemberIdAndDateBetweenOrderByDate(
-						memberId, LocalDate.of(year, month, 1), LocalDate.of(year, month, 1).plusMonths(1));
+						memberId,
+						LocalDate.of(year, month, 1).minusDays(1),
+						LocalDate.of(year, month, 1).plusMonths(1));
 
 		// 요약된 데이터를 바탕으로 일별 데이터 변환
 		List<GetMonthlyDetailResponse.Day> daily =
